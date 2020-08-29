6117aec547eb707cc516cdf90a729eac1cdbb12b
Joel Berger <joelberger@gmail.com>
2020-08-29 14:52:04 -0400

    Added ctags support

--- bin/Deploy Drafts Scripts.sh
+++ bin/Deploy Drafts Scripts.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 rsync -a --exclude .git/ ~/Code/drafts-scripts/Scripts/ ~/Library/Mobile\ Documents/iCloud\~com\~agiletortoise\~Drafts5/Documents/Library/Scripts/ --delete
+
+ctags --excmd=number --tag-relative=no --fields=+a+m+n+S -f /Users/joel/Library/Application\ Support/BBEdit/Completion\ Data/JavaScript/drafts-scripts.tags -R /Users/joel/Code/drafts-scripts/Scripts >/dev/null 2>&1
