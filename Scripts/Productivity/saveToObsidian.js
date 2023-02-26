3c057fd4e8768d7a565ea502d14de5f8e1a7cfca
Joel Berger <joelberger@gmail.com>
2023-02-26 22:01:34 +0000

    [Save to Obsidian] Frontmatter and special chars

--- Scripts/Productivity/saveToObsidian.js
+++ Scripts/Productivity/saveToObsidian.js
@@ -1,22 +1,44 @@
 // saveToObsidian.js
 // Save Draft to Obsidian with Date YAML
-// TODO: Add YAML Date frontmatter and Meeting Note formatting?
+// TODO: None
 
 const baseURL = "obsidian://new";
 let vaultName = getObsidianVault();
 
+const specialCharRegex = new RegExp("[:\\/]", "g");
+const dateRegex = new RegExp("\\d{4}-\\d{2}-\\d{2}$");
+let obsidianFilename = draft.displayTitle.replace(specialCharRegex, "-");
+let content = draft.content;
+
+// Populate frontmatter
+let datestamp = obsidianFilename.match(dateRegex);
+if (datestamp || draft.tags.length) {
+   let frontmatter = "---\n";
+
+   if (datestamp) {
+      frontmatter += "date: " + datestamp + "\n";
+   }
+
+   if (draft.tags.length) {
+      frontmatter += "tags: " + draft.tags.toString().replace(",", ", ") + "\n";
+   }
+
+   frontmatter += "---\n\n";
+   content = frontmatter + content;
+}
+
 let cb = CallbackURL.create();
 cb.baseURL = baseURL;
-cb.addParameter( "vault", vaultName);
-cb.addParameter( "name", draft.displayTitle);
-cb.addParameter("content", draft.content);
+cb.addParameter("vault", vaultName);
+cb.addParameter("name", obsidianFilename);
+cb.addParameter("content", content);
 cb.waitForResponse = false;
 let success = cb.open();
-                                  
+
 if (success) {
    console.log("Draft successfully saved to Obsidian");
 }
-else { 
+else {
    console.log("Save to Obsidian failed");
    context.fail();
 }
@@ -27,6 +49,6 @@ function getObsidianVault() {
    let credential = Credential.create("Obsidian Vault", "Obsidian Vault");
    credential.addTextField("obsidianVault", "Obsidian Vault");
    credential.authorize();
-   
-   return credential.getValue("obsidianVault");  
+
+   return credential.getValue("obsidianVault");
 }
