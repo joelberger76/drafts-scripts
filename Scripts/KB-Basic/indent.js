3766c1e882b15ff03ab4ee9ebcaa9a635df387f2
Joel Berger <joelberger@gmail.com>
2024-03-04 09:38:02 -0500

    Updated indent/outdent per agiletortise latest

--- Scripts/KB-Basic/indent.js
+++ Scripts/KB-Basic/indent.js
@@ -10,35 +10,42 @@ else if (draft.languageGrammar.toLowerCase().includes("markdown")) {
 }
 
 // grab ranges and text
-let [selStart, selLen] = editor.getSelectedRange();
-let [lnStart, lnLen] = editor.getSelectedLineRange();
-let lnText = editor.getTextInRange(lnStart, lnLen);
+let [selStart, selLen] = editor.getSelectedRange()
+let [lnStart, lnLen] = editor.getSelectedLineRange()
+let lnText = editor.getTextInRange(lnStart, lnLen)
 
 // loop over lines and add indents skipping blank lines
-let indentedLines = [];
-let indentCt = 0;
-let fl = false;
+let indentedLines = []
+let indentCt = 0
+let fl = false; // track line ending
 if (lnText.endsWith("\n")) {
-	lnText = lnText.slice(0, -1);
-	fl = true;
+	lnText = lnText.slice(0, -1)
+	fl = true
 }
-let lines = lnText.split("\n");
-for(let line of lines) {
-	if (line.length > 0) {
-		indentedLines.push(indent + line);
-		indentCt++;
-	}
-	else {
-		indentedLines.push(line);
-	}
+let lines = lnText.split("\n")
+if (lines.length == 1 && lines[0].length == 0 && selLen == 0) {
+	// cursor on a single empty line, just add indent
+	editor.setTextInRange(selStart, 0, indent)
+	editor.setSelectedRange(selStart+indent.length, 0)
 }
+else {
+	for(let line of lines) {
+		if (line.length > 0) { // do not indent blank line
+			indentedLines.push(indent + line)
+			indentCt++
+		}
+		else {
+			indentedLines.push(line)
+		}
+	}
 
-// set text
-let resultText = indentedLines.join("\n");
-if (fl) { resultText = resultText + "\n"; }
-editor.setTextInRange(lnStart, lnLen, resultText);
+	// set text
+	let resultText = indentedLines.join("\n")
+	if (fl) { resultText = resultText + "\n" } // restore last ending
+	editor.setTextInRange(lnStart, lnLen, resultText)
 
-// update selection
-let newStart = selStart + indent.length;
-let newLen = selLen + (indent.length * (indentCt - 1))
-editor.setSelectedRange(newStart, newLen);
+	// update selection
+	let newStart = selStart + indent.length
+	let newLen = selLen + (indent.length * (indentCt - 1))
+	editor.setSelectedRange(newStart, newLen)
+}
--- Scripts/KB-Basic/outdent.js
+++ Scripts/KB-Basic/outdent.js
@@ -10,39 +10,61 @@ else if (draft.languageGrammar.toLowerCase().includes("markdown")) {
 }
 
 // grab ranges and text
-let [selStart, selLen] = editor.getSelectedRange();
-let [lnStart, lnLen] = editor.getSelectedLineRange();
-let lnText = editor.getTextInRange(lnStart, lnLen);
+let [selStart, selLen] = editor.getSelectedRange()
+let [lnStart, lnLen] = editor.getSelectedLineRange()
+let lnText = editor.getTextInRange(lnStart, lnLen)
 
 // loop over lines and remove level of indents
-let updatedLines = [];
-let outdentCt = 0;
-let startAdjust = 0;
-let isFirst = true;
+let updatedLines = []
+let outdentCt = 0
+let startAdjust = 0
+let isFirst = true
 
-let lines = lnText.split("\n");
+let lines = lnText.split("\n")
 for (let line of lines) {
 	if (line.startsWith(indent)) {
-   		updatedLines.push(line.substr(indent.length, line.length-indent.length));
+   		updatedLines.push(line.substr(indent.length, line.length-indent.length))
    		if (!isFirst) {
-      		outdentCt++;
+      		outdentCt += indent.length
     	}
     	else {
     		if (selStart > lnStart) {
-	      		startAdjust = indent.length;
+	      		startAdjust = indent.length
+	      	}
+    	}
+  }
+  else if (line.startsWith("\t")) { // also clean up other whitespace that might not match prefs
+      updatedLines.push(line.substr(1, line.length-1))
+   		if (!isFirst) {
+      		outdentCt += 1
+    	}
+    	else {
+    		if (selStart > lnStart) {
+	      		startAdjust = 1
+	      	}
+    	}
+  }
+  else if (line.startsWith("  ")) { // also clean up other whitespace that might not match prefs
+      updatedLines.push(line.substr(2, line.length-2))
+   		if (!isFirst) {
+      		outdentCt += 2
+    	}
+    	else {
+    		if (selStart > lnStart) {
+	      		startAdjust = 2
 	      	}
     	}
   }
   else { // not indented
-    updatedLines.push(line);
+    updatedLines.push(line)
   }
-  isFirst = false;
+  isFirst = false
 }
 
 // set text
-editor.setTextInRange(lnStart, lnLen, updatedLines.join("\n"));
+editor.setTextInRange(lnStart, lnLen, updatedLines.join("\n"))
 
 // update selection
-let newStart = selStart - startAdjust;
-let newLen = selLen - (indent.length * outdentCt)
-editor.setSelectedRange(newStart, newLen);
+let newStart = selStart - startAdjust
+let newLen = selLen - outdentCt
+editor.setSelectedRange(newStart, newLen)
